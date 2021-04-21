faefab94b3be90e52c155d3bff40a2e23851e207
Andrew Lawendy <andrew.lawendy@outlook.com>
2021-04-21 10:27:00 +0200

    feat: :sparkles: add more rules

--- src/rules/marginRuleSet.ts
+++ src/rules/marginRuleSet.ts
@@ -12,6 +12,46 @@ const rules: CssSmellingRuleSet = {
             "margin-top has no effect on an element with a display inline",
         },
       },
+      {
+        prop: "position",
+        value: "absolute",
+        message: {
+          type: "error",
+          content:
+            "margin-top has no effect on an element with a position absolute as it's now out of the flow",
+        },
+      },
+      {
+        prop: "position",
+        value: "fixed",
+        message: {
+          type: "error",
+          content:
+            "margin-top has no effect on an element with a position fixed as it's now out of the flow",
+        },
+      },
+    ],
+  },
+  marginRight: {
+    conflicts: [
+      {
+        prop: "position",
+        value: "absolute",
+        message: {
+          type: "error",
+          content:
+            "margin-right has no effect on an element with a position absolute as it's now out of the flow",
+        },
+      },
+      {
+        prop: "position",
+        value: "fixed",
+        message: {
+          type: "error",
+          content:
+            "margin-right has no effect on an element with a position fixed as it's now out of the flow",
+        },
+      },
     ],
   },
   marginBottom: {
@@ -23,6 +63,46 @@ const rules: CssSmellingRuleSet = {
           type: "error",
           content:
             "margin-bottom has no effect on an element with a display inline",
+        },
+      },
+      {
+        prop: "position",
+        value: "absolute",
+        message: {
+          type: "error",
+          content:
+            "margin-bottom has no effect on an element with a position absolute as it's now out of the flow",
+        },
+      },
+      {
+        prop: "position",
+        value: "fixed",
+        message: {
+          type: "error",
+          content:
+            "margin-bottom has no effect on an element with a position fixed as it's now out of the flow",
+        },
+      },
+    ],
+  },
+  marginLeft: {
+    conflicts: [
+      {
+        prop: "position",
+        value: "absolute",
+        message: {
+          type: "error",
+          content:
+            "margin-left has no effect on an element with a position absolute as it's now out of the flow",
+        },
+      },
+      {
+        prop: "position",
+        value: "fixed",
+        message: {
+          type: "error",
+          content:
+            "margin-left has no effect on an element with a position fixed as it's now out of the flow",
         },
       },
     ],
